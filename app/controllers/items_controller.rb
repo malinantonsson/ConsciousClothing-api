ca750ee8e1bb51ccaaf5007356873aac63457276
Malin Antonsson <malin@malinantonsson.com>
2019-05-31 14:22:26 +0100

    add items api

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,44 @@
+# app/controllers/items_controller.rb
 class ItemsController < ApplicationController
+  before_action :set_outfit, only: [:show, :update, :destroy]
+
+  # GET /items
+  def index
+    @items = Item.all
+    json_response(@items)
+  end
+
+  # POST /items
+  def create
+    @item = Item.create!(item_params)
+    json_response(@item, :created)
+  end
+
+  # GET /items/:id
+  def show
+    json_response(@item)
+  end
+
+  # PUT /items/:id
+  def update
+    @item.update(item_params)
+    head :no_content
+  end
+
+  # DELETE /items/:id
+  def destroy
+    @item.destroy
+    head :no_content
+  end
+
+  private
+
+  def item_params
+    # whitelist params
+    params.permit(:name)
+  end
+
+  def set_outfit
+    @item = Item.find(params[:id])
+  end
 end
